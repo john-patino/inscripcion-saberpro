35b6d10b2f4f3c39226b61bdafdb752eb6283adc
johnpatino <johnpatino@unicesar.edu.co>
2025-03-18 21:01:08 -0500

    ajuste approutes

--- src/Core/Routes/appRoutes.jsx
+++ src/Core/Routes/appRoutes.jsx
@@ -1,5 +1,5 @@
 // src/Core/Routes/appRoutes.jsx
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import PublicRoutes from "./publicRoutes";
 import AuthRoutes from "./authRoutes";
 import PrivateRoutes from "./privateRoutes";
@@ -8,9 +8,16 @@ import NotFound from "../App/public/pages/notFound404";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/*" element={<PublicRoutes />} />
+      {/* Redireccionar raíz a dashboard */}
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+
+      {/* Rutas públicas */}
       <Route path="/auth/*" element={<AuthRoutes />} />
+
+      {/* Rutas privadas protegidas */}
       <Route path="/dashboard/*" element={<PrivateRoutes />} />
+
+      {/* Página no encontrada */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
